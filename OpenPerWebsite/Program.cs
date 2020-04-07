bc3dc9c6b8d08536ca3efc6ee8f042050536bf8e
phamduyloc <locpdd.it@gmail.com>
2020-04-07 14:11:14 +0700

    Add project files.

---
+++ OpenPerWebsite/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace OpenPerWebsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            { 
+
+            }
+            Console.WriteLine("Hello world");
+            Console.Read();
+        }
+    }
+}
